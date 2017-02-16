22af93f0e0d93511bd5d43310e1238afdebd6a9d
up201504327 <up201504327@B03AZ0514.salas.feupsig.fe.up.pt>
2017-02-16 16:29:11 +0000

    fim da 1ra tp

--- LPOa1/src/maze/Map.java
+++ LPOa1/src/maze/Map.java
@@ -1,6 +1,7 @@
 package maze;
 
 public class Map {
+	public int herox, heroy;
 	public static void main(String arg[]){
 		char[][] map = new char[][]	{ 		
 			{'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'},
@@ -14,16 +15,45 @@ public class Map {
 			{'X', 0, 'I', 0, 'I', 0, 'X', 'k', 0, 'X'},
 			{'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'}
 		};
+		
 		Map m = new Map();
 		m.printmap(map);
+		
 	}
+	////
 	
 	public void printmap(char[][] map){
+		
 		for (int i = 0; i < map.length; i++) {
 			for (int j = 0; j < map[i].length; j++) {
 				System.out.print(map[i][j] + " ");
 			}
 			System.out.println();
 		}
+	}
+	/////
+	
+	public void userinput(){
+		System.out.println("Up(U), down(D), left(L) or right(R)?");
+		String command = System.in.toString();
+		switch(command){
+		case "u": 
+			break;
+		case "d":
+			break;
+		case "l":
+			break;
+		case "r":
+			break;
+		default: System.out.println("Invalid Command, please try again.");
+				command=null;
+				userinput();
+		break;
+		}
+	}
+	/////
+	public void Movement(){
+		
+		
 	}
 }
